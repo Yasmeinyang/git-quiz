cfddbc1f689bea40e89edecc3b26be3e5730d8b8
Yasmein Yang <114441657+GO4YAS@users.noreply.github.com>
2023-01-10 18:47:14 -0500

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,101 @@
-// Assignment code here
+var formEl = document.querySelector("#highscore");
+var highscoreEl = document.querySelector("#highscore-panel")
+var instructionsEl = document.querySelector('#instructions');
+var timerEl = document.querySelector('#countdown');
+var questionEl = document.querySelector('#quiz-question');
+var quizEl = document.querySelector('#quiz-body');
+var statusEl = document.querySelector('#status')
+var startBtn = document.querySelector('#start');
+var timeLeft = 0;
+var timeInterval;
 
-// Function to capture the proper password length or continue to prompt
-var pwLength = function() {
-	lengthValue = prompt("Choose Password Length Between: 8 - 128");
-	// prompt cannot be empty, be less than 8, more than 128 and cannot contain text
-	while (lengthValue === "" || parseInt(lengthValue) < 8 || parseInt(lengthValue) > 128 || isNaN(parseInt(lengthValue))) {
-		lengthValue = prompt("Choose Password Length Between: 8 - 128");
-	}
-	console.log("Password length: " + lengthValue);
-	return lengthValue;
+// Start Questions Array
+var questionObj = [
+    {
+        question: "Commonly used data types do NOT include:",
+        options: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: "3"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ______",
+        options: ["Other Arrays", "Numbers and Strings", "Booleans", "All of the Above"],
+        answer: "4"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _____",
+        options: ["Quotes", "Parenthesis", "Square Brackets", "Curly Brackets"],
+        answer: "2",
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        options: ["Quotes", "Curly Brackets", "Commas", "Parenthesis"],
+        answer: "1"
+    },
+    {
+        question: "A useful tool used during development/debugging for printing content to the debugger is:",
+        options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "4"
+    },
+]
+
+// Display opening instructions
+quizEl.textContent = "Try to answer the following code-related questions withn the time limit. Keep in mind that incorrect answers will penalize your score / time by ten seconds!";
+
+// Timer function set at 60 seconds
+function timer() {
+    timeLeft = 60;
+    startBtn.setAttribute("style", "display: none"); // Hide start button on start
+
+    timeInterval = setInterval(function () {
+        // While timer is running
+        if (timeLeft > -1) {
+            timerEl.textContent = 'Time: ' + timeLeft; // Display timer on page
+            timeLeft--;
+        }
+        // After timer hits zero
+        else {
+            timerEl.textContent = 'Times Up!'; // Display times up message
+            clearInterval(timeInterval); // Reset Timer
+            endGame(timeLeft);
+        }
+    }, 1000);
+    questions();
 }
 
-function generatePassword() {
-  
-  passwordCharacters = "";
-  newPassword = "";
-	var passwordLength = pwLength(); // Length of Password Requestd
-	var lowerCase = window.confirm("Lowercase Letters?"); // Use Lowercase Letters?
-	var upperCase = window.confirm("Uppercase Letters?"); // Use Uppercase Letters?
-	var numbers = window.confirm("Numbers?"); // Use Numbers?
-  var special = window.confirm("Special Characters?"); // Use Special Characters?
-  
-  // If no character options are selected, alert message and begin again.
-  if (lowerCase === false && upperCase === false && numbers === false && special === false) {
-    window.alert("Please select at least one password criteria.");
-    generatePassword();
-  }
-  else { // Build password characters string per requirements.
-    if (lowerCase) {
-      passwordCharacters = "abcdefghijklmnopqrstuvwxyz";
-    }
-    if (upperCase) {
-      passwordCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    }
-    if (numbers) {
-      passwordCharacters += "0123456789";
-    }
-    if (special) {
-      passwordCharacters += " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-    }
-       
-    console.log("Use Characters: " + passwordCharacters);
+// Create question groups
+var questions = function () {
+    quizEl.textContent = "";
+    for (let i = 0; i < questionObj.length; i++) {
+        var panelEl = document.createElement("section");
+        panelEl.className = "question-panel";
+        panelEl.setAttribute("id", "panel" + [i + 1]);
+        quizEl.appendChild(panelEl);
 
-    for (var i = 0; i < passwordLength; i++) {
-      newPassword += passwordCharacters.charAt(Math.floor(Math.random() * passwordCharacters.length));
-    }
-  }
-	console.log("Pasword is: " + newPassword);
-	return newPassword;
-}
+        var questionEl = document.createElement("h3");
+        questionEl.className = "quiz-question";
+        questionEl.textContent = questionObj[i].question;
+        panelEl.appendChild(questionEl);
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+        var answerEl = document.createElement("div");
+        answerEl.setAttribute("data-question-id", "1"); // Set question option id
+        answerEl.setAttribute("data-answer-id", questionObj[i].answer); // set correct answer id
+        answerEl.setAttribute("data-panel-id", "panel" + [i + 1]); // set question group id
+        answerEl.className = "answer-choice";
+        answerEl.textContent = "1. " + questionObj[i].options[0];
+        panelEl.appendChild(answerEl);
 
-// Write password to the #password input
-function writePassword() {
-	var password = generatePassword();
-	var passwordText = document.querySelector("#password");
+        var answerEl = document.createElement("div");
+        answerEl.setAttribute("data-question-id", "2"); // Set question option id
+        answerEl.setAttribute("data-answer-id", questionObj[i].answer); // set correct answer id
+        answerEl.setAttribute("data-panel-id", "panel" + [i + 1]); // set question group id
+        answerEl.className = "answer-choice";
+        answerEl.textContent = "2. " + questionObj[i].options[1];
+        panelEl.appendChild(answerEl);
 
-	passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+        var answerEl = document.createElement("div");
+        answerEl.setAttribute("data-question-id", "3"); // Set question option id
+        answerEl.setAttribute("data-answer-id", questionObj[i].answer); // set correct answer id
+        answerEl.setAttribute("data-panel-id", "panel" + [i + 1]); // set question group id
+        answerEl.className = "answer-choice";
+        answerEl.textContent = "3. " + questionObj[i].options[2];
+        panelEl.appendChild(answerEl);
